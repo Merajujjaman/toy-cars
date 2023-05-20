25425babc4f616c166fa10550e51c19b064b6a25
Merajujjaman <asmmerajujjaman@gmail.com>
2023-05-20 22:13:35 +0600

    car card create done

---
+++ src/pages/Home/CarsCard.jsx
@@ -0,0 +1,25 @@
+
+const CarsCard = ({ singleCar }) => {
+    const { toy_name, price, rating, picture } = singleCar
+    return (
+
+        <div className="card w-full bg-base-100 shadow-xl">
+
+            <figure><img src={picture} className="object-cover h-40 rounded-xl" alt="toys photo not found" /></figure>
+
+            <div className="card-body">
+                <h2 className="card-title">
+                    {toy_name}
+                    <div className="badge badge-secondary">Raring: {rating}</div>
+                </h2>
+                <p className="text-green-500">price: {price}</p>
+                <div className="card-actions justify-end">
+                    <div className="btn btn-outline">Details</div>
+                </div>
+            </div>
+        </div>
+
+    );
+};
+
+export default CarsCard;
